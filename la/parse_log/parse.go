8f66b08eeb8431effb5843033265fb17d9c00817
Sharad Vikram <sharad.vikram@gmail.com>
2016-11-22 17:19:24 -0800

    updated scripts a bit

--- la/parse_log/parse.go
+++ la/parse_log/parse.go
@@ -59,26 +59,41 @@ func parseLine(line string) BoardLogMessage {
 	return logMessage
 }
 
-func printMessage(message BoardLogMessage) {
+func printMessage(message BoardLogMessage, negCount *int) {
 	if message.Conf.PTV == -1 {
 		return
 	}
+	if message.Rt < 0 {
+		*negCount++
+	}
 	rng := float64(message.Raw.Rng)
-	fmt.Printf("%d,%s,%f,%f,%f,%f,%f,%f,%f,%f\n", message.Rt, message.Rts, convertRaw(message.Raw.S1A, rng)-
-		convertRaw(message.Raw.S1W, rng), convertRaw(message.Raw.S2A, rng)-
-		convertRaw(message.Raw.S2W, rng), convertRaw(message.Raw.S3A, rng)-
-		convertRaw(message.Raw.S3W, rng), message.Hu_pr.BT, message.Hu_pr.BP, message.Hu_pr.HH, convertRaw(message.Raw.PT, rng), convertRaw(message.Raw.NC, rng))
+	fmt.Printf("%d,%s,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f\n",
+		message.Rt,
+		message.Rts,
+		convertRaw(message.Raw.S1A, rng),
+		convertRaw(message.Raw.S1W, rng),
+		convertRaw(message.Raw.S2A, rng),
+		convertRaw(message.Raw.S2W, rng),
+		convertRaw(message.Raw.S3A, rng),
+		convertRaw(message.Raw.S3W, rng),
+		message.Hu_pr.BT, message.Hu_pr.BP, message.Hu_pr.HH,
+		convertRaw(message.Raw.PT, rng), convertRaw(message.Raw.NC, rng))
 }
 
 func parseFile(path string) {
-	fmt.Println("unixtime,datetime,no2,o3,co,temperature,pressure,humidity,pt,nc")
+	fmt.Println("unixtime,datetime,no2-A,no2-W,o3-A,o3-W,co-A,co-W,temperature,pressure,humidity,pt,nc")
 	file, err := os.Open(path)
+	totalCount := 0
+	negCount := 0
 	util.CheckErr(err)
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		printMessage(parseLine(scanner.Text()))
+		printMessage(parseLine(scanner.Text()), &negCount)
+		totalCount++
 	}
+	util.Log.Infof("Parsed %d lines.", totalCount)
+	util.Log.Warningf("Found %d negative timestamps.", negCount)
 	util.CheckErr(scanner.Err())
 
 }
